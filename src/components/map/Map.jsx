7c90b6348f88e192aa6dd9b9a57194b69b2de9a7
AB <austinbuhler@gmail.com>
2020-11-18 11:10:40 -0500

    Map rendering with park markers

--- src/components/map/Map.jsx
+++ src/components/map/Map.jsx
@@ -1,7 +1,80 @@
-import React from 'react'
+import React, { useState } from 'react'
+import {
+  GoogleMap,
+  LoadScript,
+  Marker,
+  InfoWindow,
+  StreetViewPanorama,
+} from '@react-google-maps/api'
+import { connect } from 'react-redux'
 
-const Map = () => {
-  return <h1>Map HERE</h1>
+const Map = ({ parks }) => {
+  const [selectedPark, setSelectedPark] = useState(null)
+
+  const mapContainerStyle = {
+    width: '50vw',
+    height: '90vh',
+  }
+  const center = {
+    lat: 40.73061,
+    lng: -73.935242,
+  }
+
+  const options = {
+    disableDefaultUI: true,
+    zoomControl: true,
+  }
+
+  const renderParks = () => {
+    return parks.map((park) => {
+      return (
+        <Marker
+          key={park.id}
+          position={{
+            lat: parseFloat(park.latitude),
+            lng: parseFloat(park.longitude),
+          }}
+          onClick={() => setSelectedPark(park)}
+        />
+      )
+    })
+  }
+
+  return (
+    <LoadScript googleMapsApiKey={process.env.REACT_APP_GOOGLE_MAPS_API_KEY}>
+      <GoogleMap
+        mapContainerStyle={mapContainerStyle}
+        zoom={13}
+        center={center}
+        options={options}
+      >
+        {renderParks()}
+        {selectedPark && (
+          <InfoWindow
+            position={{
+              lat: parseFloat(selectedPark.latitude),
+              lng: parseFloat(selectedPark.longitude),
+            }}
+            onCloseClick={() => setSelectedPark(null)}
+          >
+            <div>
+              <h4>{selectedPark.name}</h4>
+              <p>{selectedPark.location}</p>
+              <a href={selectedPark.website} target='_blank'>
+                {selectedPark.website}
+              </a>
+            </div>
+          </InfoWindow>
+        )}
+      </GoogleMap>
+    </LoadScript>
+  )
 }
 
-export default Map
+const mapStateToProps = (state) => {
+  return {
+    parks: state.parks,
+  }
+}
+
+export default connect(mapStateToProps)(Map)
