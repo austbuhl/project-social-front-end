4e32050d3a10a8792c76d0a705ebbae708dfcbd6
AB <austinbuhler@gmail.com>
2020-11-24 12:03:41 -0500

    Message popup up and running

--- src/components/events/RSVPButton.jsx
+++ src/components/events/RSVPButton.jsx
@@ -1,24 +1,48 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { connect } from 'react-redux'
-import { Button } from 'semantic-ui-react'
+import { Button, Message } from 'semantic-ui-react'
 import { attendEvent } from '../../redux/actions'
 import { useHistory } from 'react-router-dom'
+import { selectCurrentUserEvents } from '../../redux/selectors'
 
-const RSVPButton = ({ eventId, loggedIn, attendEvent }) => {
+const RSVPButton = ({ eventId, loggedIn, attendEvent, currentUserEvents }) => {
   const history = useHistory()
+  const [alreadyAttending, setAlreadyAttending] = useState(false)
+
+  useEffect(() => {
+    let interval = setInterval(() => {
+      setAlreadyAttending(false)
+    }, 3000)
+    return () => clearTimeout(interval)
+  }, [alreadyAttending])
+
   const clickHandler = () => {
-    if (loggedIn) {
-      attendEvent({ event_id: eventId })
+    if (!loggedIn) history.push('/login', history.location.pathname)
+
+    if (currentUserEvents.find((event) => event.id === eventId)) {
+      setAlreadyAttending(true)
     } else {
-      history.push('/login', history.location.pathname)
+      attendEvent({ event_id: eventId })
     }
   }
 
-  return <Button content='RSVP' onClick={clickHandler} />
+  return (
+    <>
+      <Button secondary onClick={clickHandler}>
+        <Button.Content>RSVP</Button.Content>
+      </Button>
+      {alreadyAttending && (
+        <Message negative>
+          <Message.Header>You're already attending!</Message.Header>
+        </Message>
+      )}
+    </>
+  )
 }
 const mapStateToProps = (state) => {
   return {
     loggedIn: state.loggedIn,
+    currentUserEvents: selectCurrentUserEvents(state),
   }
 }
 
