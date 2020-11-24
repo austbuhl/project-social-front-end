acc9b88f54ac189ade10638723459295c6ded3c9
AB <austinbuhler@gmail.com>
2020-11-23 22:18:24 -0500

    Comments List Toggling

--- src/components/comments/CommentsList.jsx
+++ src/components/comments/CommentsList.jsx
@@ -1,16 +1,29 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import CommentDetail from './CommentDetail'
 import { Comment, Header, Button } from 'semantic-ui-react'
 
 const CommentsList = ({ comments }) => {
-  const commentsToShow = -3
-  const commentsHidden = comments.length + commentsToShow
+  const commentsToShow = comments.length < 3 ? comments.length : 3
   const sortedComments = comments.sort((a, b) => a.id - b.id)
+  const initialComment = comments.length - commentsToShow
+
+  const [firstComment, setFirstComment] = useState(initialComment)
+
+  useEffect(() => {
+    setFirstComment(initialComment)
+  }, [comments])
+
+  console.log(firstComment)
+  console.log(comments)
+  const indexOfLastComment = firstComment + 3
 
   const renderComments = () => {
-    return sortedComments
-      .slice(commentsToShow)
-      .map((comment) => <CommentDetail key={comment.id} comment={comment} />)
+    return (
+      sortedComments
+        .slice(firstComment, indexOfLastComment)
+        // .slice(commentsToShow)
+        .map((comment) => <CommentDetail key={comment.id} comment={comment} />)
+    )
   }
 
   return (
@@ -19,8 +32,12 @@ const CommentsList = ({ comments }) => {
         <Header as='h3' dividing>
           Comments
         </Header>
-        {commentsHidden}
-        <Button>Load Previous Comments</Button>
+        {firstComment > 0 && (
+          <Button onClick={() => setFirstComment(firstComment - 1)}>
+            Load Previous Comments
+          </Button>
+        )}
+
         {renderComments()}
       </Comment.Group>
     </div>
